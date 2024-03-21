0845d5e419f999f219d574225894dab1c1d1425a
Snuskasaurus <julien.rogel@gmail.com>
2024-03-22 00:42:22 +0100

    Websocket connection

--- Program.cs
+++ Program.cs
@@ -19,7 +19,26 @@ namespace Wolcen
     class Server
     {
         static string CONFIG_IP = "127.0.0.1";
-        static int CONFIG_PORT = 7600;
+        static int CONFIG_PORT = 6666;
+
+        public static void RespondToWebSocketHandshake(ref NetworkStream Stream, String DataReceived)
+        {
+            const string eol = "\r\n"; // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+
+            byte[] response = Encoding.UTF8.GetBytes("HTTP/1.1 101 Switching Protocols" + eol
+                + "Connection: Upgrade" + eol
+                + "Upgrade: websocket" + eol
+                + "Sec-WebSocket-Accept: " + Convert.ToBase64String(
+                    System.Security.Cryptography.SHA1.Create().ComputeHash(
+                        Encoding.UTF8.GetBytes(
+                            new System.Text.RegularExpressions.Regex("Sec-WebSocket-Key: (.*)").Match(DataReceived).Groups[1].Value.Trim() + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+                        )
+                    )
+                ) + eol
+                + eol);
+
+            Stream.Write(response, 0, response.Length);
+        }
 
         public static void Execute()
         {
@@ -43,9 +62,16 @@ namespace Wolcen
 
                 stream.Read(bytes, 0, bytes.Length);
                 String data = Encoding.UTF8.GetString(bytes);
-
                 Console.WriteLine("Data Received fromn client:\n{0}", data);
 
+                if (new System.Text.RegularExpressions.Regex("^GET").IsMatch(data))
+                {
+                    RespondToWebSocketHandshake(ref stream, data);
+                }
+                else
+                {
+
+                }
 
             }
         }
