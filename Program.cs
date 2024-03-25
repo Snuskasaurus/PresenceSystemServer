6209a80610d58872767c05d3cfbc60e6f7ec401a
Snuskasaurus <julien.rogel@gmail.com>
2024-03-25 12:05:07 +0100

    Added a responses to hello
    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -48,10 +48,70 @@ namespace Wolcen
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        private static Byte[] EncodeMessageToSend(String message)
+        {
+            Byte[] response;
+            Byte[] bytesRaw = Encoding.UTF8.GetBytes(message);
+            Byte[] frame = new Byte[10];
+
+            Int32 indexStartRawData = -1;
+            Int32 length = bytesRaw.Length;
+
+            frame[0] = (Byte)129;
+            if (length <= 125)
+            {
+                frame[1] = (Byte)length;
+                indexStartRawData = 2;
+            }
+            else if (length >= 126 && length <= 65535)
+            {
+                frame[1] = (Byte)126;
+                frame[2] = (Byte)((length >> 8) & 255);
+                frame[3] = (Byte)(length & 255);
+                indexStartRawData = 4;
+            }
+            else
+            {
+                frame[1] = (Byte)127;
+                frame[2] = (Byte)((length >> 56) & 255);
+                frame[3] = (Byte)((length >> 48) & 255);
+                frame[4] = (Byte)((length >> 40) & 255);
+                frame[5] = (Byte)((length >> 32) & 255);
+                frame[6] = (Byte)((length >> 24) & 255);
+                frame[7] = (Byte)((length >> 16) & 255);
+                frame[8] = (Byte)((length >> 8) & 255);
+                frame[9] = (Byte)(length & 255);
+
+                indexStartRawData = 10;
+            }
+
+            response = new Byte[indexStartRawData + length];
+
+            Int32 i, reponseIdx = 0;
+
+            //Add the frame bytes to the reponse
+            for (i = 0; i < indexStartRawData; i++)
+            {
+                response[reponseIdx] = frame[i];
+                reponseIdx++;
+            }
+
+            //Add the data bytes to the response
+            for (i = 0; i < length; i++)
+            {
+                response[reponseIdx] = bytesRaw[i];
+                reponseIdx++;
+            }
+
+            return response;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public static void SendMessageToClient(ref NetworkStream Stream, String Message)
         {
-            const string eol = "\r\n"; // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
-            byte[] EncodedMessage = Encoding.UTF8.GetBytes(Message + eol);
+            //const string eol = "\r\n";
+            byte[] EncodedMessage = EncodeMessageToSend(Message);
             Stream.Write(EncodedMessage, 0, EncodedMessage.Length);
         }
 
@@ -111,6 +171,9 @@ namespace Wolcen
 
                         string text = Encoding.UTF8.GetString(decoded);
                         Console.WriteLine("Message received:\n{0}\n", text);
+
+                        string messageToSend = "Message \"" + text + "\" received commander";
+                        Server.SendMessageToClient(ref stream, messageToSend);
                     }
                 }
             }
